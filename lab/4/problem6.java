d1277f0dda877c35f96cc64d23f74608f9a1a33a
Cloud-Link-VII <38575038+Cloud-Link-VII@users.noreply.github.com>
2018-09-29 02:23:31 -0400

    Update problem6.java

--- lab/4/problem6.java
+++ lab/4/problem6.java
@@ -9,14 +9,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        ArrayList pos = new ArrayList();
+        ArrayList<Integer> pos = new ArrayList<Integer>();
 
         for(int i = 0; i < 10; i++)
         {
             pos.add((int)(Math.random() * 10 + 1));
         }
-
-        pos.add(-1);
 
         for(int i = 1; i <= 10; i++)
         {
@@ -25,24 +23,22 @@ public class Main
 
         System.out.println(pos);
 
-        endOfInsert(3, pos, 10);
+        specificInsert(3, pos, 10);
         specificInsert(7, pos, 100);
 
     }
 
-    public static void endOfInsert(ArrayList arr, int insert)
+    public static void endOfInsert(ArrayList<Integer> arr, int insert)
     {
         arr.add(insert);
+      	System.out.println(arr);
     }
 
-    public static void specificInsert(int pos, ArrayList arr, int insert)
+    public static void specificInsert(int pos, ArrayList<Integer> arr, int insert)
     {
         arr.add(pos, insert);
+      	System.out.println(arr);
     }
 }
-
-
-
-
 
 
